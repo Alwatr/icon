9207a0d572262eacf043f999293a775d1b8419b1
S. Amir Mohammad Najafi <njfamirm@gmail.com>
2023-09-24 16:48:04 +0330

    feat(icon-set-material): add cleanup script

--- packages/icon-set-matrial/cleanup.sh
+++ packages/icon-set-matrial/cleanup.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
 
-rename 's/_/-/g' *
+rm -rf svg
+mkdir -p svg/outline
+mkdir svg/round
+mkdir svg/sharp
 
-mkdir outline
-mkdir rounded
-mkdir sharp
-
-for icon_dir in ./*; do
-
-  icon_name=$(basename "$icon_dir")
-
-  for sub_dir in "$icon_dir"/*; do
-
-    sub_dir_name=$(basename "$sub_dir")
-
-    svg_path="$sub_dir/${icon_name}_24px.svg"
-
-    if [ -f "$svg_path" ]; then
-      case "$sub_dir_name" in
-      "materialsymbolsoutlined")
-        cp "$svg_path" "outline/$icon_name-24.svg"
-        ;;
-      "materialsymbolsrounded")
-        cp "$svg_path" "rounded/$icon_name-24.svg"
-        ;;
-      "materialsymbolssharp")
-        cp "$svg_path" "sharp/$icon_name-24.svg"
-        ;;
-      esac
-    fi
-  done
+cd original
+for icon in ./*; do
+  cp -av ./${icon}/materialsymbolsoutlined/${icon}_24px.svg ../svg/outline/${icon}.svg
+  cp -av ./${icon}/materialsymbolsrounded/${icon}_24px.svg ../svg/round/${icon}.svg
+  cp -av ./${icon}/materialsymbolssharp/${icon}_24px.svg ../svg/sharp/${icon}.svg
 done
+
+cd ../svg
+rename 's/_/-/g' **/**
